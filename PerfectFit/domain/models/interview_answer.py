04eb9841b842f8e30da1df3d3d7eee6383218d57
seungyong <seungyong20@naver.com>
2024-11-05 03:11:39 +0900

    기본키명 변경

--- PerfectFit/domain/models/interview_answer.py
+++ PerfectFit/domain/models/interview_answer.py
@@ -13,7 +13,7 @@ if TYPE_CHECKING:
 class InterviewAnswer(db.Model):
     __tablename__ = "interview_answer"
 
-    improvement_id: Mapped[int] = mapped_column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
+    interview_answer_id: Mapped[int] = mapped_column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
     question_id: Mapped[int] = mapped_column(
         INTEGER(unsigned=True),
         ForeignKey("interview_question.question_id", onupdate="CASCADE", ondelete="CASCADE"),
