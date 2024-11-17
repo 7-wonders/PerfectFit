e45dc667e51b0ef4220c1879493222d0459d35d6
JeongseokHan <115478278+JeongseokHan@users.noreply.github.com>
2024-11-17 23:23:07 +0900

    자기소개서 상세보기 디자인 개발
    자기소개서 상세보기 디자인만 개발하였고, 아직 기능은 구현 되지 않았습니다.

--- PerfectFit/controllers/resume_controller.py
+++ PerfectFit/controllers/resume_controller.py
@@ -33,3 +33,11 @@ def get_resume_list(page):
     pageRange = 5  # 한 번에 표시할 페이지 범위
     totalPages = (totalItems + pageRange - 1) // pageRange  # 전체 페이지 수 계산
     return render_template("resume_list.html", page=page, totalPages=totalPages, pageRange=pageRange)
+
+@resume_bp.route('/detail')
+def get_resume_detail():
+    page = 1;
+    totalItems = 100  # 예시: 전체 항목의 수
+    pageRange = 5  # 한 번에 표시할 페이지 범위
+    totalPages = (totalItems + pageRange - 1) // pageRange  # 전체 페이지 수 계산
+    return render_template("resume_detail.html", page=page, totalPages=totalPages, pageRange=pageRange)
