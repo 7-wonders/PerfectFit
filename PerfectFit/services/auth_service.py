53654210a26267861a82247930165aabf853674b
seungyong <seungyong20@naver.com>
2024-11-19 11:49:19 +0900

    SQLAlchemy 세션 범위 지정
    1. COMMIT을 하지 않아, 계속 데이터가 롤백 되었음.
    
    2. COMMIT을 하면 데이터가 정상적으로 들어가지만, ROLLBACK이라는 로그가 발생
    
    3. get_session을 통해 새로운 세션을 받아와 여러 개의 세션을 사용하는 현상이였음.
    
    4. with 문을 통헤 session 하나로 사용하고, 구문이 끝나면 자동으로 닫게 처리

--- PerfectFit/services/auth_service.py
+++ PerfectFit/services/auth_service.py
@@ -1,7 +1,8 @@
-import os
-from typing import Optional
+from typing import Optional, cast
 
 from flask import request
+from flask_sqlalchemy.session import Session
+from sqlalchemy.orm import scoped_session
 
 from constants.sns_kind import SnsKind
 from config.config_mysql import get_session
@@ -43,108 +44,122 @@ def _delete_refresh_token():
 class AuthService:
     @staticmethod
     def google_login(code: str) -> dict:
-        access_token, token_type = GoogleOAuthHandler().get_access_token(code)
-        user_response_json = GoogleOAuthHandler().get_user_info(token_type, access_token)
+        with get_session() as session:
+            session = cast(scoped_session[Session], session)
+            access_token, token_type = GoogleOAuthHandler().get_access_token(code)
+            user_response_json = GoogleOAuthHandler().get_user_info(token_type, access_token)
 
-        sns_id = user_response_json.get('id')
-        email = user_response_json.get('email')
-        name = user_response_json.get('name')
-        profile_image = user_response_json.get('picture')
+            sns_id = user_response_json.get('id')
+            email = user_response_json.get('email')
+            name = user_response_json.get('name')
+            profile_image = user_response_json.get('picture')
 
-        if not _check_enough_info(sns_id, name):
-            logger.error(f'User Request Error\n'
-                         f'sns_id : {sns_id} | email : {email} | name : {name}')
-            raise CustomException(ExceptionType.GOOGLE_NOT_ENOUGH_INFO)
+            if not _check_enough_info(sns_id, name):
+                logger.error(f'User Request Error\n'
+                             f'sns_id : {sns_id} | email : {email} | name : {name}')
+                raise CustomException(ExceptionType.GOOGLE_NOT_ENOUGH_INFO)
 
-        already_user: Optional[AppUser] = get_session().query(AppUser).filter(AppUser.sns_id == sns_id).first()
-        if already_user:
+            already_user: Optional[AppUser] = session.query(AppUser).filter(AppUser.sns_id == sns_id).first()
+            if already_user:
+                _delete_refresh_token()
+                return _create_token(already_user.user_id)
+
+            user = AppUser(sns_id=sns_id, sns_kind=SnsKind.GOOGLE.value, email=email, username=name,
+                           profile_path=profile_image)
+            session.add(user)
+            session.flush()
+
+            logger.info(f'Google Signup Success\n'
+                        f'sns_id : {sns_id} | email : {email} | name : {name} | profile_image : {profile_image}')
+
+            token_info = _create_token(user.user_id)
             _delete_refresh_token()
-            return _create_token(already_user.user_id)
 
-        user = AppUser(sns_id=sns_id, sns_kind=SnsKind.GOOGLE.value, email=email, username=name,
-                       profile_path=profile_image)
-        get_session().add(user)
-        get_session().flush()
+            session.commit()
 
-        logger.info(f'Google Signup Success\n'
-                    f'sns_id : {sns_id} | email : {email} | name : {name} | profile_image : {profile_image}')
-
-        token_info = _create_token(user.user_id)
-        _delete_refresh_token()
-
-        return token_info
+            return token_info
 
     @staticmethod
     def naver_login(code: str, state: str) -> dict:
-        access_token, token_type = NaverOAuthHandler().get_access_token(code, state=state)
-        user_response_json = NaverOAuthHandler().get_user_info(token_type, access_token)
+        with get_session() as session:
+            session = cast(scoped_session[Session], session)
 
-        sns_id = user_response_json.get('response').get('id')
-        name = user_response_json.get('response').get('name')
-        email = user_response_json.get('response').get('email')
-        profile_image = user_response_json.get('response').get('profile_image')
-        phone_number = str(user_response_json.get('response').get('mobile')).replace('-', '')
+            access_token, token_type = NaverOAuthHandler().get_access_token(code, state=state)
+            user_response_json = NaverOAuthHandler().get_user_info(token_type, access_token)
 
-        if not _check_enough_info(sns_id, name):
-            logger.error(f'User Request Error\n'
-                         f'sns_id : {sns_id} | email : {email} | name : {name} | profile_image : {profile_image} | phone_number : {phone_number}')
-            raise CustomException(ExceptionType.NAVER_NOT_ENOUGH_INFO)
+            sns_id = user_response_json.get('response').get('id')
+            name = user_response_json.get('response').get('name')
+            email = user_response_json.get('response').get('email')
+            profile_image = user_response_json.get('response').get('profile_image')
+            phone_number = str(user_response_json.get('response').get('mobile')).replace('-', '')
 
-        already_user = get_session().query(AppUser).filter(AppUser.sns_id == sns_id).first()
-        if already_user:
+            if not _check_enough_info(sns_id, name):
+                logger.error(f'User Request Error\n'
+                             f'sns_id : {sns_id} | email : {email} | name : {name} | profile_image : {profile_image} | phone_number : {phone_number}')
+                raise CustomException(ExceptionType.NAVER_NOT_ENOUGH_INFO)
+
+            already_user = session.query(AppUser).filter(AppUser.sns_id == sns_id).first()
+            if already_user:
+                _delete_refresh_token()
+                return _create_token(already_user.user_id)
+
+            # MM-DD 형식
+            response_birthday = user_response_json.get('response').get('birthday')
+            birth_year = user_response_json.get('response').get('birthyear')
+            birth_month, birth_day = map(int, response_birthday.split('-'))
+            age = get_age(int(birth_year), birth_month, birth_day)
+
+            user = AppUser(sns_id=sns_id, sns_kind=SnsKind.NAVER.value, email=email, username=name,
+                           profile_path=profile_image, phone_number=phone_number, age=age)
+            session.add(user)
+            session.flush()
+
+            logger.info(f'Naver Signup Success\n'
+                        f'sns_id : {sns_id} | email : {email} | name : {name} | profile_image : {profile_image} | phone_number : {phone_number} | age : {age}')
+
+            token_info = _create_token(user.user_id)
             _delete_refresh_token()
-            return _create_token(already_user.user_id)
 
-        # MM-DD 형식
-        response_birthday = user_response_json.get('response').get('birthday')
-        birth_year = user_response_json.get('response').get('birthyear')
-        birth_month, birth_day = map(int, response_birthday.split('-'))
-        age = get_age(int(birth_year), birth_month, birth_day)
+            session.commit()
 
-        user = AppUser(sns_id=sns_id, sns_kind=SnsKind.NAVER.value, email=email, username=name,
-                       profile_path=profile_image, phone_number=phone_number, age=age)
-        get_session().add(user)
-        get_session().flush()
-
-        logger.info(f'Naver Signup Success\n'
-                    f'sns_id : {sns_id} | email : {email} | name : {name} | profile_image : {profile_image} | phone_number : {phone_number} | age : {age}')
-
-        token_info = _create_token(user.user_id)
-        _delete_refresh_token()
-
-        return token_info
+            return token_info
 
     @staticmethod
     def kakao_login(code: str, state: str) -> dict:
-        access_token, token_type = KakaoOAuthHandler().get_access_token(code, state=state)
-        user_response_json = KakaoOAuthHandler().get_user_info(token_type, access_token)
+        with get_session() as session:
+            session = cast(scoped_session[Session], session)
 
-        sns_id = user_response_json.get('id')
-        name = user_response_json.get('properties').get('nickname')
-        profile_image = user_response_json.get('properties').get('profile_image')
+            access_token, token_type = KakaoOAuthHandler().get_access_token(code, state=state)
+            user_response_json = KakaoOAuthHandler().get_user_info(token_type, access_token)
 
-        if not _check_enough_info(sns_id, name):
-            logger.error(f'User Request Error\n'
-                         f'sns_id : {sns_id} | name : {name} | profile_image : {profile_image}')
-            raise CustomException(ExceptionType.KAKAO_NOT_ENOUGH_INFO)
+            sns_id = user_response_json.get('id')
+            name = user_response_json.get('properties').get('nickname')
+            profile_image = user_response_json.get('properties').get('profile_image')
 
-        already_user = get_session().query(AppUser).filter(AppUser.sns_id == sns_id).first()
+            if not _check_enough_info(sns_id, name):
+                logger.error(f'User Request Error\n'
+                             f'sns_id : {sns_id} | name : {name} | profile_image : {profile_image}')
+                raise CustomException(ExceptionType.KAKAO_NOT_ENOUGH_INFO)
 
-        if already_user:
+            already_user = session.query(AppUser).filter(AppUser.sns_id == sns_id).first()
+
+            if already_user:
+                _delete_refresh_token()
+                return _create_token(already_user.user_id)
+
+            user = AppUser(sns_id=sns_id, sns_kind=SnsKind.KAKAO.value, username=name, profile_path=profile_image)
+            session.add(user)
+            session.flush()
+
+            logger.info(f'Kakao Signup Success\n'
+                        f'sns_id : {sns_id} | name : {name} | profile_image : {profile_image}')
+
+            token_info = _create_token(user.user_id)
             _delete_refresh_token()
-            return _create_token(already_user.user_id)
 
-        user = AppUser(sns_id=sns_id, sns_kind=SnsKind.KAKAO.value, username=name, profile_path=profile_image)
-        get_session().add(user)
-        get_session().flush()
+            session.commit()
 
-        logger.info(f'Kakao Signup Success\n'
-                    f'sns_id : {sns_id} | name : {name} | profile_image : {profile_image}')
-
-        token_info = _create_token(user.user_id)
-        _delete_refresh_token()
-
-        return token_info
+            return token_info
 
     @staticmethod
     def renew_token(refresh_token: str) -> dict:
