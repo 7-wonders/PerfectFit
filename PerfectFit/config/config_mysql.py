ea44fbfd831dbf0526e22a7f92593b768a63955c
Minuffy <128578002+Minu0803@users.noreply.github.com>
2024-11-21 22:17:01 +0900

    testusers.html 파일 생성 후 테스트 중 + config_mysql.py 에 SERVER_NAME = os.getenv('DEVELOP_SERVER_NAME') or '127.0.0.1:5000'이 부분 맥북용으로 임시 변경

--- PerfectFit/config/config_mysql.py
+++ PerfectFit/config/config_mysql.py
@@ -29,6 +29,6 @@ class Config:
     else:
         SQLALCHEMY_TRACK_MODIFICATIONS = False
         SQLALCHEMY_ECHO = True
-        SERVER_NAME = os.getenv('DEVELOP_SERVER_NAME') or 'localhost:5000'
+        SERVER_NAME = os.getenv('DEVELOP_SERVER_NAME') or '127.0.0.1:5000' ## 맥북용으로 임시 변경
 
     SECRET_KEY = os.environ.get('FLASH_SECRET_KEY')
