34d0393cf290c8e2f919492289645634f98383a1
dbstjdqls14 <dbstjdqls14@naver.com>
2024-12-09 02:55:35 +0900

    선택정보 해결

--- PerfectFit/services/optionalinfo_service.py
+++ PerfectFit/services/optionalinfo_service.py
@@ -1,11 +1,13 @@
-from domain.models import ProjectExperience, WorkExperience, ProjectExperienceTask
+from domain.models.project_experience import ProjectExperience
+from domain.models.project_experience_task import ProjectExperienceTask
+from domain.models.work_experience import WorkExperience
 from domain.models.app_user import AppUser
 from config.config_mysql import get_session
 
 class OptionalInfoService:
     @staticmethod
     def register_info(user_id: int, major: str, university: str, university_status: str,
-                      grade: str, project_experiences: list, work_experiences: list, phone_number: str):
+                      grade: str, phone_number: str):
 
         with get_session() as session:
             # 사용자의 선택 정보를 업데이트합니다.
@@ -21,58 +23,58 @@ class OptionalInfoService:
                 session.commit()
 
     @staticmethod
-    def register_projectexp(user_id: int, major: str, university: str, university_status: str,
-                      grade: str, project_experiences: list, work_experiences: list, phone_number: str):
+    def register_projectexp(user_id: int, project_experiences: list):
 
         with get_session() as session:
             # 사용자의 선택 정보를 업데이트합니다.
             user = session.query(AppUser).filter(AppUser.user_id == user_id).first()
 
-            if user:
+            for project in user.project_experiences:
+                for task in project.tasks:
+                    session.delete(task)
+                session.delete(project)
 
-                user.project_experiences.clear()  # 기존 프로젝트 경험 삭제
-                new_projects = []
-                for project in project_experiences:
-                    new_project = ProjectExperience(
-                        user_id=user_id,
-                        project_name=project["projectName"],
-                        from_date=project["fromDate"],
-                        to_date=project["toDate"],
+            for project in project_experiences:
+                # 프로젝트 경험 객체 생성
+                new_project = ProjectExperience(
+                    user_id=int(user_id),
+                    project_name=str(project["projectName"]),
+                    from_date=str(project["fromDate"]),
+                    to_date=str(project["toDate"]),
+                )
+
+                session.add(new_project)
+                session.flush()
+
+                for content in project["contents"]:
+                    new_task = ProjectExperienceTask(
+                        project_experience_id=int(new_project.project_experience_id),
+                        content=str(content),
                     )
-                    session.add(new_project)
-                    new_projects.append(new_project)
-                    # user_id
-                    session.flush()  # ID를 얻기 위해 flush() 호출 None
-                    # 각 content를 ProjectExperienceTask로 생성
-                    for content in project["contents"]:  # contents는 str[] 형태
-                        new_task = ProjectExperienceTask(
-                            project_experience_id=new_project.project_experience_id,
-                            content=content,
-                        )
-                        session.add(new_task)
-                session.commit()
+                    session.add(new_task)
+
+            session.commit()
 
     @staticmethod
-    def register_workexp(user_id: int, major: str, university: str, university_status: str,
-                      grade: str, project_experiences: list, work_experiences: list, phone_number: str):
+    def register_workexp(user_id: int, work_experiences: list):
 
         with get_session() as session:
             # 사용자의 선택 정보를 업데이트합니다.
             user = session.query(AppUser).filter(AppUser.user_id == user_id).first()
 
-            if user:
-                # 직장 경험 정보 업데이트
-                user.work_experiences.clear()  # 기존 직장 경험 삭제
-                for work in work_experiences:
-                    new_work = WorkExperience(
-                        user_id=user_id,
-                        company_name=work["companyName"],
-                        from_date=work["fromDate"],
-                        to_date=work["toDate"],
-                        position=work["position"],
-                        responsibility=work["responsibility"],
-                        reason=work["reason"]
-                    )
-                    session.add(new_work)
+            for work in user.work_experiences:
+                session.delete(work)
 
-                session.commit()
+            for work in work_experiences:
+                new_work = WorkExperience(
+                    user_id=int(user_id),
+                    company_name=str(work["companyName"]),
+                    from_date=str(work["fromDate"]),
+                    to_date=str(work["toDate"]),
+                    position=str(work["position"]),
+                    responsibility=str(work["responsibility"]),
+                    reason=str(work["reason"])
+                )
+                session.add(new_work)
+
+            session.commit()
